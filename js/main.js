85f3acbb514f1229e42221844ea8cbfdf63c5bde
Roneilla <bumanlar@sheridancollege.ca>
2020-01-04 00:20:33 -0500

    Edited style

--- js/main.js
+++ js/main.js
@@ -184,3 +184,13 @@ jQuery(document).ready(function ($) {
   }
 
 });
+
+$('a[href^=#]').on("click",function(e){
+    var t= $(this.hash);
+    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+    if(t.length){
+        var tOffset=t.offset().top;
+        $('html,body').animate({scrollTop:tOffset-20}, 'slow', 'swing');
+        e.preventDefault();
+    }
+});
